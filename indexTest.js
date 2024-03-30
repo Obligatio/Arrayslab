8ef8f0e362c966b7dc86ba5708cd76fcde519866
kevin <kevinke386@gmail.com>
2024-03-30 19:57:16 +0300

    initial commit

---
+++ indexTest.js
@@ -0,0 +1,32 @@
+// Write your solution here!
+ const cats = ["Milo", "Otis", "Garfield"];
+ function destructivelyAppendCat(){
+    cats.push('Ralph');
+ }
+ function destructivelyPrependCat(){
+    cats.unshift('Bob');
+ }
+ function destructivelyRemoveLastCat(){
+    cats.pop();
+ }
+ function destructivelyRemoveFirstCat(){
+    cats.shift();
+ }
+ function appendCat(name){
+    let copyCats = [...cats,name];
+    return copyCats;
+ }
+function prependCat(name){
+    let copyCats = [name,...cats];
+    return copyCats;
+}
+function removeLastCat(){
+    let remainderCats = cats.slice(0,2);
+    return remainderCats;
+}
+function removeFirstCat(){
+    let remainderCats = cats.slice(1);
+    return remainderCats;
+}
+
+ // Write your solution here!
